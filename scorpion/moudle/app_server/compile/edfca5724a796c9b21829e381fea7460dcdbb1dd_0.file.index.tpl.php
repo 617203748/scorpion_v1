c01cae9883637c3cf4179614b06d9a68c2456a4b
617203748 <617203748.com>
2018-11-26 10:30:38 +0800

    init

---
+++ scorpion/moudle/app_server/compile/edfca5724a796c9b21829e381fea7460dcdbb1dd_0.file.index.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 3.1.29, created on 2018-11-26 10:09:15
+  from "I:\web_www\scorpion_v1\scorpion\moudle\app_server\templates\default\index.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_5bfb55cbe0e302_11654726',
+  'file_dependency' => 
+  array (
+    'edfca5724a796c9b21829e381fea7460dcdbb1dd' => 
+    array (
+      0 => 'I:\\web_www\\scorpion_v1\\scorpion\\moudle\\app_server\\templates\\default\\index.tpl',
+      1 => 1543197647,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5bfb55cbe0e302_11654726 ($_smarty_tpl) {
+?>
+这是 index.tpl html页面<?php }
+}
